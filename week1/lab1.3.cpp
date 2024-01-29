18aff85900652b2c5f8e499fac112581b1b357a9
SKGBank <bankkanthajai@gmail.com>
2024-01-29 11:56:29 +0700

    week1

---
+++ week1/lab1.3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int *GetSet( int * ) ;
+
+int *GetSet( int * ) {
+	int *data, num ;
+	printf( "Enter the number of Member composition : " ) ;
+	scanf( "%d", &num ) ;
+
+	for( int n = 0 ; n < num ; n++ ) {
+		printf( "Enter number of Member [%d] : ", n + 1 ) ;
+		scanf( "%d", &data[ n ] ) ;
+	}
+	printf( "\n Total number of members = %d \n", num ) ;
+	for( int z = 0 ; z < num ; z++ ) {
+		printf( "\n Member informationr [%d] : %d\n", z + 1, data[ z ] ) ;
+	}
+}
+
+int main() {
+ int *data, num ;
+ data = GetSet( &num ) ;
+ return 0 ;
+}//end function
